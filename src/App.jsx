8e07a49f2aea4f6a85834bd86d81b9a8652143aa
aydisaddem <sdouma91@gmail.com>
2025-11-10 11:56:26 +0100

    fixing react project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Landing from "./landing/Landing.jsx";
+
+function App() {
+  return <>
+  <Landing/>
+  </>;
+}
+
+export default App;
---
+++ src/landing/Landing.jsx
@@ -0,0 +1,11 @@
+ import "./Landing.css"
+ const Landing =()=>{
+    return (
+        <>
+        <div className="container">
+        </div>
+        </>
+    )
+}
+
+export default Landing;
