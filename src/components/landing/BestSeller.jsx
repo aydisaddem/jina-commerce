ae5b41adf847492be65fb4cfbc2ca9e77157c0c9
aydisaddem <sdouma91@gmail.com>
2025-12-26 22:59:29 +0100

    finished landing page

---
+++ src/components/landing/BestSeller.jsx
@@ -0,0 +1,77 @@
+import { brands } from "../../data/brands";
+import { NavLink } from "react-router-dom";
+import { slugify } from "../../utils/slugify";
+const BestSeller = () => {
+  const bestSeller = [
+    {
+      id: "693fda658823f3ca51bb6dda",
+      picture: "https://res.cloudinary.com/dhxnksrd6/image/upload/v1765792263/Jina/sx2tqxkqspv5bmdrne0d.jpg",
+      name: "ASUS TUF Gaming A15 Gaming Laptop",
+      reference: "FA506NF-HN004",
+      category:"Laptops",
+      subCategory:"Gaming Laptop",
+      logo: brands.ASUS,
+      price: "2079",
+    },
+    {
+      id: "69451aef0b75f2b0f2ca3c20",
+      picture: "https://res.cloudinary.com/dhxnksrd6/image/upload/v1766136458/Jina/iv58aimkz9i1r5xnqc17.jpg",
+      name: "Headset with Microphone JBL Quantum",
+      reference: "96973",
+      category:"Accessories",
+      subCategory:"Headsets & Earphones",
+      logo: brands.JBL,
+      price: "989",
+    },
+    {
+      id: "6949a54a163fffe31f131380",
+      picture: "https://res.cloudinary.com/dhxnksrd6/image/upload/v1766434111/Jina/qzza7fni53ye9kx9tuu1.png",
+      name: "USB Webcam White Shark OWL Full HD",
+      reference: "GWC-004",
+      category:"Accessories",
+      subCategory:"Webcams",
+      logo: brands["WHITE SHARK"],
+      price: "80",
+    },
+    {
+      id: "693fd81d8823f3ca51bb6add",
+      picture: "https://res.cloudinary.com/dhxnksrd6/image/upload/v1765791566/Jina/npbppmeecdjg0fxybcv3.png",
+      name: "Gaming Laptop MSI Thin 15 B13U",
+      reference: "B13UCX-2607XFR-SS",
+      category:"Laptops",
+      subCategory:"Gaming Laptop",
+      logo: brands.MSI,
+      price: "2025",
+    },
+  ];
+
+  return (
+    <div className="bestSeller-section">
+      <div className="bestSeller-header">
+        <h2>Our Best Sellers</h2>
+        <NavLink to="/products" className="view-all-link" >View All Products &gt;</NavLink>
+      </div>
+
+      <div id="bestSeller-container">
+        {bestSeller.map((product) => (
+            <div key={product.id} className="bestSeller-card">
+                            <NavLink to={`/products/${slugify(product.category)}/${slugify(product.subCategory)}/preview/${product.id}`} style={{ all: "unset" }}>
+
+            <img src={product.picture} alt={product.name} className="bestSeller-img" />
+            <div className="bestSeller-info">
+              <img src={product.logo} alt={product.name + " logo"} className="bestSeller-logo" />
+              <h3>{product.name}</h3>
+              <p>Ref: {product.reference}</p>
+              <p className="bestSeller-price">{product.price},000 DT</p>
+            </div>
+                        </NavLink>
+
+          </div>
+          
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,19 +1,30 @@
-import '../../styles/landing.css';
-import MainCarousel from './MainCarousel';
-import BrandCarousel from './BrandCarousel';
-import { NavLink } from 'react-router-dom';
+import "../../styles/landing.css";
+import MainCarousel from "./MainCarousel";
+import BrandCarousel from "./BrandCarousel";
+import BestSeller from "./BestSeller";
+import phoneIcon from "../../assets/phone-white.png";
+import cashIcon from "../../assets/cash-white2.png";
+import houseIcon from "../../assets/house-white.png";
 const Landing = () => {
-
-
-  
   return (
-
-      <div id="landing">
-        <MainCarousel />
-        <BrandCarousel/>
-     
+    <div id="landing">
+      <MainCarousel />
+      <BrandCarousel />
+      <BestSeller />
+      <div className="service-info">
+        <div className="service-item">
+          <img src={houseIcon} alt="Home delivery" /> <p>Home delivery</p>
+        </div>
+        <div className="service-item">
+          <img src={phoneIcon} alt="Commercial service" />
+          <p>Support <span>44 404 404</span></p>
+        </div>
+        <div className="service-item">
+          <img src={cashIcon} alt="Cash on delivery" />
+          <p>Cash on delivery</p>
+        </div>
       </div>
-
+    </div>
   );
 };
 
