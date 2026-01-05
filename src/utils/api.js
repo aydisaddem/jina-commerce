11dea41be1aadb1ad9341fe1082c1479df2d5cb6
aydisaddem <sdouma91@gmail.com>
2026-01-05 16:01:19 +0100

    auth style

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 // ✅ Use environment variable or fallback to localhost for development
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-console.log(import.meta.env.VITE_API_URL)
 
 const api = axios.create({
   baseURL: API_BASE_URL,
